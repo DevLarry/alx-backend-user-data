e6d66ef088486b189be4a5a349fa0fc4bd832ee5
DevLarry <olanrewajusamad070@gmail.com>
2023-11-17 02:06:51 +0100

    Database implimentation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,7 +35,7 @@ class DB:
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
-        self._session.flush()
+        self._session.commit()
         return user
 
 
